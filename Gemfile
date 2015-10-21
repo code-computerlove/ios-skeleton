095aca6f2db27e3c6b569be9a98bb0385732b2d3
Tony McBeth <tony.mcbeth@gmail.com>
2015-10-21 15:19:05 +0100

    added deployment files

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem 'rake', '~> 10.4'
+gem 'cocoapods', '~> 0.38'
+gem 'fastlane', '~> 1.12'
---
+++ src/[[PROJECT_NAME]]/Modules/Main/View/MainViewController.swift
@@ -0,0 +1,21 @@
+import Foundation
+import UIKit
+
+
+class MainViewController: UIViewController, MainModuleViewProtocol {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		
+	}
+	
+	
+	//MainModuleViewProtocol Functions
+	
+	func testModuleSetup() {
+		
+		let alertController = AlertController()
+		
+	}
+}
