b383ffcacc351c61c9ee2a7911f2e03206c4db1f
Tony McBeth <tony.mcbeth@gmail.com>
2015-12-08 11:18:45 +0000

    updated tests

--- src/[[PROJECT_NAME]]Tests/DolphinSpec.swift
+++ src/[[PROJECT_NAME]]Tests/DolphinSpec.swift
@@ -1,21 +1,11 @@
-//
-//  DolphinSpec.swift
-//  [[PROJECT_NAME]]
-//
-// Example Quick and Nimble test file
-
 import Quick
 import Nimble
 
+
 class DolphinSpec: QuickSpec {
+    
     override func spec() {
-        
-        describe("a dolphin") {
-            
-            beforeEach {
-                
-            }
-            
+        describe("a dolphin") {            
             describe("click") {
                 context("When it's not near anything") {
                     it ("emits only one click") {
