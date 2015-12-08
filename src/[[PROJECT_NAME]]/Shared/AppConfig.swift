cf590dc465cc35585022c403108acf593fcf8e76
Tony McBeth <tony.mcbeth@gmail.com>
2015-12-08 10:20:51 +0000

    Updated app config

--- src/[[PROJECT_NAME]]/Shared/AppConfig.swift
+++ src/[[PROJECT_NAME]]/Shared/AppConfig.swift
@@ -8,20 +8,48 @@ class AppConfig: NSObject {
 	static func create() -> AppConfig {
 		
 		let appConfig = AppConfig()
+		createAppFileStructure(appConfig)
+		
+		return appConfig;
+	}
+	
+	private static func createAppFileStructure(appConfig: AppConfig) {
 		
 		let paths: NSArray = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true);
-		let contentDirPath = paths[0].stringByAppendingPathComponent("content");
+		var isDir : ObjCBool = true
 		
-		if !NSFileManager.defaultManager().fileExistsAtPath(contentDirPath) {
-			
+		let contentDirPath = paths[0].stringByAppendingPathComponent("content");
+		if !NSFileManager.defaultManager().fileExistsAtPath(contentDirPath, isDirectory: &isDir) {
 			try! NSFileManager.defaultManager().createDirectoryAtPath(contentDirPath, withIntermediateDirectories: false, attributes: nil)
-			
-			if let appContentFilePath = NSURL(fileURLWithPath: contentDirPath).URLByAppendingPathComponent("content.json").path {
-				
-				appConfig.appContentFilePath = appContentFilePath
-			}
 		}
-
-		return appConfig
+		
+		let mediaDirPath =  paths[0].stringByAppendingPathComponent("content/media");
+		if !NSFileManager.defaultManager().fileExistsAtPath(mediaDirPath, isDirectory: &isDir) {
+			try! NSFileManager.defaultManager().createDirectoryAtPath(mediaDirPath, withIntermediateDirectories: false, attributes: nil)
+		}
+		
+		let contentDirUrl = NSURL(fileURLWithPath: contentDirPath)
+		
+		appConfig.appContentFilePath =
+			getFilePath(contentDirUrl, filename: "content.json", createFromBundle: false)
+	}
+	
+	private static func getFilePath(contentDirUrl: NSURL, filename: String, createFromBundle: Bool) -> String {
+		
+		if let filePath = contentDirUrl.URLByAppendingPathComponent(filename).path {
+			if createFromBundle && !NSFileManager.defaultManager().fileExistsAtPath(filePath) {
+				
+				let bundle = NSBundle.mainBundle()
+				let filenameParts = filename.characters.split{$0 == "."}.map(String.init)
+				
+				if let bundlePath = bundle.pathForResource(filenameParts[0], ofType: filenameParts[1]) {
+					try! NSFileManager.defaultManager().copyItemAtPath(bundlePath, toPath: filePath)
+				}
+			}
+			
+			return filePath
+		}
+		
+		return ""
 	}
 }
